b2b3485abb1963c70d78a999d324b0faf14deb45
Volodymyr Yaropud <bratiskavova@gmail.com>
2024-03-08 11:08:38 +0200

    Add project files.

---
+++ CheckFile/FileChange.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CheckFile
+{
+    internal class FileChange
+    {
+        public readonly FileSystemWatcher watcher;
+        public FileChange()
+        {
+            watcher = new FileSystemWatcher("C:\\folder2\\CheckFile\\CheckFile", "*.json");
+            watcher.NotifyFilter = NotifyFilters.LastWrite;
+            watcher.Changed += OnChanged;
+            watcher.EnableRaisingEvents = true;
+        }
+
+        private void OnChanged(object sender, FileSystemEventArgs e)
+        {
+            var jsonString = new JsonReader().Read();
+
+            var data = new MySerialization().SerializeTo(jsonString);
+           e.
+            PrintMessage.Print("some message", data);
+        }
+    }
+}
